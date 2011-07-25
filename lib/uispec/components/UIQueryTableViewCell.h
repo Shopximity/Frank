68c6e7cac6ef6d0bf04fdff691d2d743f6f0596c
Tyler <haikusw@gmail.com>
2011-07-24 17:35:29 -0700

    I don't know where these changes came from; I didn't make them (some script?).

--- lib/uispec/components/UIQueryTableViewCell.h
+++ lib/uispec/components/UIQueryTableViewCell.h
@@ -12,6 +12,6 @@
 
 }
 
--(UIQuery *)delete;
+-(void)delete;
 
 @end
--- lib/uispec/components/UIQueryTableViewCell.m
+++ lib/uispec/components/UIQueryTableViewCell.m
@@ -4,11 +4,9 @@
 
 @implementation UIQueryTableViewCell
 
--(UIQuery *)delete {
-
+-(void)delete {
 	UITableView *tableView = self.parent.tableView;
 	[tableView.dataSource tableView:tableView commitEditingStyle:UITableViewCellEditingStyleDelete forRowAtIndexPath:[tableView indexPathForCell:[self.views objectAtIndex:0]]];
-	return self.parent;
 }
 
 @end
